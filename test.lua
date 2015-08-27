5466c4e568221820f1439454d8c71b325471a719
Alex Sandro <geniofuturo@gmail.com>
2015-08-27 12:50:22 -0300

    update test.

--- test.lua
+++ test.lua
@@ -5,4 +5,13 @@
 -- Time: 12:29
 -- To change this template use File | Settings | File Templates.
 --
+local root_dir = getenv("ROOT_DIR")
 
+local handle, msg = loadlib(getenv("LIBRARY_PATH"))
+print(handle, msg)
+
+if (not handle or handle == -1) then
+    error(msg)
+end
+
+callfromlib(handle, 'lua_larchiveopen')
